658a58d9e90418a87399decc3c9081b816707ddb
NathanNuckels <48299408+NathanNuckels@users.noreply.github.com>
2021-08-01 20:08:46 -0500

    Finished balance text

--- Form1.cs
+++ Form1.cs
@@ -12,9 +12,33 @@ namespace Blackjack
 {
     public partial class Form1 : Form
     {
+        //create objects
+        Label balance = new Label();
+        TextBox betBox = new TextBox();
+        Button betButton = new Button();
+        int bal=100;
         public Form1()
         {
             InitializeComponent();
+            //set form size
+            this.MinimumSize = new Size(500,500);
+            this.Size = new Size(500,500);
+            this.MaximumSize = new Size(500,500);
+            this.Text = ".NET Blackjack";
+            //add objects
+            this.Controls.Add(betButton);
+            this.Controls.Add(betBox);
+            this.Controls.Add(balance);
+            System.Diagnostics.Debug.WriteLine(bal.ToString()); //testing .ToString()
+            balance.Text = bal.ToString();
+            balance.Font = new Font("Helvetica",20);
+            balance.AutoSize = false;
+            balance.TextAlign = ContentAlignment.TopCenter;
+            balance.Location = new Point(125,250);
+            balance.Size = new Size(250,50);
+
+            
+            
         }
 
     }
